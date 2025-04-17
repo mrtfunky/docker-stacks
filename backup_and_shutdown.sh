b9a9497df86b148340f06eb19b8ebf7dfe2b4bac
mrt <54836057+mrtfunky@users.noreply.github.com>
2025-04-17 19:35:42 +0200

    Switch to borgbackup

--- backup_and_shutdown.sh
+++ backup_and_shutdown.sh
@@ -5,8 +5,7 @@
 # Get the directory of the script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$SCRIPT_DIR/stop_stacks.sh
-$SCRIPT_DIR/backup_stacks.sh
+$SCRIPT_DIR/borgbackup/borgbackup_volumes.sh
 
 echo "Poweroff for 7 hours"
 sudo /usr/sbin/rtcwake -m off -s 25200
